5af9f48e530c02697467e6a3f8d8592b4e37aa93
Karl Svartholm <karl@int3software.com>
2021-02-18 18:51:40 +0100

    read OSC Stream from Muse Monitor (android app)

---
+++ index.js
@@ -0,0 +1,27 @@
+const osc = require('osc-min')
+const udp = require('dgram')
+
+const derp = {}
+const sock = udp.createSocket("udp4", function(msg, rinfo) {
+  try {
+    const data = osc.fromBuffer(msg)
+    data.elements.map(element => {
+      derp[element.address] = element.args.map(arg => arg.value)
+    })
+    return
+  } catch (error) {
+    return console.log('invalid OSC packet', error);
+  }
+});
+
+let last = 0
+setInterval(() => {
+  process.stdout.write('\033c')
+  last = 0
+  for (const [key, val] of Object.entries(derp)) {
+    process.stdout.write(`${key.replace('/elements/', '/')}:\t ${val.join(', \t')}\n`)
+    last++
+  }
+}, 100)
+
+sock.bind(7980);
